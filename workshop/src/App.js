cf786690fe0c5940b0896a1e27683445f7d74383
odanielaugusto <158776621+odanielaugusto@users.noreply.github.com>
2024-03-07 20:29:04 -0300

    Fazendo meu primeiro react

---
+++ workshop/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className= "border-2 border-red-500">
+    <h1 className= "text-bold text-blue-300">Hello, world!</h1>
+    <p className = "text-underline">Olá inteli</p>
+   </div>
+  );
+}
+
+export default App;
